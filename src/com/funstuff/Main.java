19220959453c24c76587c63f01bb4a9ef792ae5d
LszQ0 <michallech94@gmail.com>
2023-02-10 14:22:01 +0100

    Christmas tree made with asterisk signs

--- src/com/funstuff/Main.java
+++ src/com/funstuff/Main.java
@@ -5,8 +5,8 @@ public class Main {
     public static void main(String[] args) {
 
 
-    for (int i = 0; i < 7; i++) {
-        for (int j = 0; j < 14; j++) {
+    for (int i = 0; i < 7; i++) {   //how high tree will be
+        for (int j = 0; j < 14; j++) {  //how wide bottom level will be (it's 2 * tree high)
             if (j < 7 - i || j > 7 + i) {
                 System.out.print(" ");
             }
